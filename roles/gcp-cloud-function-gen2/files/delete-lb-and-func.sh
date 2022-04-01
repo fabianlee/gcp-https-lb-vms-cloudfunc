6fa43f353d096d4263a960a3d0f6f0ed112a8b58
Fabian Lee <flee999@hotmail.com>
2022-04-01 10:37:54 -0400

    tear down scripts

---
+++ roles/gcp-cloud-function-gen2/files/delete-lb-and-func.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Deletes GCP HTTPS LB components, internal and external
+#
+
+funcname=maintgen2
+channel=beta
+region=us-east1
+location_flag="--global"
+
+gcloud compute forwarding-rules delete ${funcname}-frontend $location_flag --quiet
+gcloud compute target-https-proxies delete ${funcname}-https-proxy $location_flag --quiet
+gcloud compute url-maps delete ${funcname}-lb1 $location_flag --quiet
+gcloud compute backend-services delete ${funcname}-backend $location_flag --quiet
+echo "there are no health-checks for serverless neg"
+gcloud compute ssl-certificates delete ${funcname}-lbcert $location_flag --quiet
+gcloud compute network-endpoint-groups delete ${funcname}-neg --region=$region --quiet
+
+# need to specify gen2
+gcloud $channel functions delete ${funcname} --region=$region --gen2 --quiet
+
+
+
