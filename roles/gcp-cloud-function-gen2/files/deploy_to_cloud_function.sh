12a19470a86dd3fd3e2230ed9ca0b9047aa8afb1
Fabian Lee <flee999@hotmail.com>
2023-04-18 07:05:46 -0400

    for deploying cloud function

---
+++ roles/gcp-cloud-function-gen2/files/deploy_to_cloud_function.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Creates Python gen2 Cloud Function
+#
+
+funcname=maintgen2
+entry_point=maintenance_switch
+channel=beta
+
+# enable GCP project level services
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+gcloud services enable cloudfunctions.googleapis.com cloudbuild.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com run.googleapis.com artifactregistry.googleapis.com containerregistry.googleapis.com
+
+region=$(gcloud config get compute/region)
+project_id=$(gcloud config get project)
+[[ (-n "$region") && (-n "$project_id") ]] || { echo "ERROR could not pull region or project id"; exit 1; }
+
+set -x
+
+gcloud artifacts repositories create my-repo --location=$region --repository-format=docker
+
+gcloud $channel functions deploy $funcname --entry-point=$entry_point --gen2 --runtime python310 --trigger-http --allow-unauthenticated --min-instances=1 --region=$region --set-env-vars="MAINTENANCE_MESSAGE=This is the Cloud Function maintenance message" --quiet
+
+gcloud $channel run services update $funcname --concurrency 100 --cpu=1 --region=$region
+
+set +x
+
+# test curl
+curl_url=$(gcloud $channel functions describe $funcname --format "value(serviceConfig.uri)" --region=$region --gen2)
+echo "Cloud Function at: $curl_url"
+curl $curl_url
+
+
